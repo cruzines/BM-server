e3c7be88bf3057ade4132d5d8f66efc5ec296dae
Ricardo Nunes <ricardo.agn.nunes@gmail.com>
2021-12-09 12:12:29 +0000

    new routes

--- models/User.model.js
+++ models/User.model.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 
-let UserSchema = new Schema({
+let UserSchema = new Schema({git addEventListener.
   firstName: String,
   lastName: String, 
   email: {
