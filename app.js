9f76c291822046cda9210ef2093b6cf703717d19
Maria Farran Colell <mafacoll@hotmail.com>
2021-12-12 18:10:18 +0100

    changes

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(session({
     maxAge: 1000 * 24* 60 * 60 // your cookie will be cleared after these seconds
   },
   store: MongoStore.create({
-    mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/BidMasters",
+    mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/BidMastersP3",
     // Time to Live for sessions in DB. After that time it will delete it!
     ttl: 24* 60 * 60 // your session will be cleared after these seconds
   })
