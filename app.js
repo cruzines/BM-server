87377c5bed098d7dccb861e6cf70a2cc42a12ac1
Ines Cruz <inesmncruz@gmail.com>
2021-12-14 10:47:16 +0000

    deploy

--- app.js
+++ app.js
@@ -52,6 +52,11 @@ app.use("/api", fileUploadRoutes);
 const stripeRoutes = require('./routes/stripe.routes')
 app.use("/api", stripeRoutes);
 
+app.use((req, res, next) => {
+	// If no routes match, send them the React HTML.
+	res.sendFile(__dirname + "/public/index.html");
+});
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
