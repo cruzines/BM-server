76661586da639d707c2813a2ae1de1c34e7fff95
Ines Cruz <inesmncruz@gmail.com>
2021-12-14 11:18:53 +0000

    deploy

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ app.use(session({
   })
 }));
 
-
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes");
